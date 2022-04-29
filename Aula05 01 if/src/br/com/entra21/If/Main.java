4298ba5ffe687410d63a38db577a4acfef2f3bfe
Ba-ribeiro <babi_rib@hotmail.com>
2022-04-29 19:36:48 -0300

    Aula 29/04 if

---
+++ Aula05 01 if/src/br/com/entra21/If/Main.java
@@ -0,0 +1,28 @@
+package br.com.entra21.If;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Aprendendo sobre If");
+		
+		if(false) {
+			System.out.println("Vai faazer isso quando der true");
+			System.out.println(" Somente true");
+		}
+	
+		System.out.println("Aqui vai acontecer naturalmente pq o codigo segue sempre em frente");
+		
+		if(false)  {
+			 //nunca passaria aqui pois apenas quando resulta em true passa por esse bloco
+		}
+		byte idade = 17;
+		if(idade>=18) {// é maior de idade?
+			//sim
+			System.out.println("é maior de idade");
+		}
+		
+		if( (idade>=13) && (idade<18) )  {//é adolescente (13~17)?
+			System.out.println("é adolescente");
+		}
+	}
+}
