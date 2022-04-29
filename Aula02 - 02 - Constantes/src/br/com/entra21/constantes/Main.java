eb1a1f485235f59f4a8a9dad3d875e900bceb9c0
Ba-ribeiro <babi_rib@hotmail.com>
2022-04-28 22:10:07 -0300

    Operadores Constantes

--- Aula02 - 02 - Constantes/src/br/com/entra21/constantes/Main.java
+++ Aula02 - 02 - Constantes/src/br/com/entra21/constantes/Main.java
@@ -3,11 +3,9 @@ package br.com.entra21.constantes;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		final byte idade,outraIdade;
-		idade=30;
-		outraIdade=34;
+
+
+		final byte IDADE=33;
 		
 
 	}
